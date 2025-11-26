1f47ac73d8acf6274436b71083edc42cf7754f63
Mauricio Sosa Giri <gm@free4fun.dev>
2025-11-25 22:57:25 -0300

    Implement countingValleys function to count the number of valleys in a hiking path

---
+++ problem-solving/src/countingValleys.rs
@@ -0,0 +1,15 @@
+fn countingValleys(steps: i32, path: &str) -> i32 {
+    let mut valleys = 0;
+    let mut level = 0;
+    for step in path.chars() {
+        if step == 'U' {
+            level += 1;
+            if level == 0 {  // if we arrive again to sea level we just leaved a valley
+                valleys += 1;
+            }
+        } else if step == 'D' {
+            level -= 1;
+        }
+    }
+    return valleys
+}
