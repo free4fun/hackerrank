93337fe89958348762965f7efdc7b732e1714a23
Mauricio Sosa Giri <gm@free4fun.dev>
2025-11-26 04:26:15 -0300

    Implement climbingLeaderboard function to calculate player rankings based on scores

---
+++ problem-solving/src/climbingTheLeaderboard.rs
@@ -0,0 +1,21 @@
+fn climbingLeaderboard(ranked: &[i32], player: &[i32]) -> Vec<i32> {
+    let mut result: Vec<i32> = Vec::new();
+    let mut ranked = ranked.to_vec();
+    ranked.dedup(); // Remove duplicate scores from ranked (must be sorted for dedup to work correctly)
+    for score in player.to_vec() {
+        // Remove all ranked scores less than or equal to the player's score
+        while let Some(&last) = ranked.last() {
+            print!("Last ranked score: {}", last);
+            print!(" Player score: {}", score);
+            println!();
+            if score >= last {
+                ranked.pop();
+            } else {
+                break;
+            }
+        }
+        // The player's rank is the number of remaining ranked scores + 1
+        result.push((ranked.len() + 1) as i32);
+    }
+    return result;
+}
